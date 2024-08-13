1590b1f7ec7ffd7e63e66e4118de51da3f6f21e3
skiddedruck <106821194+skiddedruck@users.noreply.github.com>
2024-08-13 07:13:38 -0400

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -8,20 +8,42 @@
   ],
   "routes": [
     {
-      "src": "/api/whitelist",
-      "dest": "/api/whitelist.json"
+      "src": "/api/getWhitelist",
+      "dest": "/functions/getWhitelist.js"
     },
     {
-      "src": "/api/update",
-      "dest": "/functions/update.js"
+      "src": "/api/updateWhitelist",
+      "dest": "/functions/updateWhitelist.js"
     },
     {
-      "src": "/api/add",
-      "dest": "/functions/add.js"
+      "src": "/api/removeWhitelist",
+      "dest": "/functions/removeWhitelist.js"
     },
     {
-      "src": "/api/remove",
-      "dest": "/functions/remove.js"
+      "src": "/api/addWhitelist",
+      "dest": "/functions/addWhitelist.js"
+    },
+    {
+      "src": "/api/unwhitelist",
+      "dest": "/functions/unwhitelist.js"
     }
-  ]
+  ],
+  "functions": {
+    "api/updateWhitelist.js": {
+      "memory": 128,
+      "maxDuration": 10
+    },
+    "api/addWhitelist.js": {
+      "memory": 128,
+      "maxDuration": 10
+    },
+    "api/removeWhitelist.js": {
+      "memory": 128,
+      "maxDuration": 10
+    },
+    "api/unwhitelist.js": {
+      "memory": 128,
+      "maxDuration": 10
+    }
+  }
 }
