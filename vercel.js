96ccd9d7c681fccfe0638365125a40ca798f7bda
skiddedruck <106821194+skiddedruck@users.noreply.github.com>
2024-08-13 05:22:30 -0400

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,12 +1,27 @@
 {
   "version": 2,
   "builds": [
-    { "src": "api/whitelist/*.json", "use": "@vercel/static" },
-    { "src": "functions/*.js", "use": "@vercel/node" }
+    {
+      "src": "functions/*.js",
+      "use": "@vercel/node"
+    }
   ],
   "routes": [
-    { "src": "/api/whitelist/update", "dest": "/functions/update.js" },
-    { "src": "/api/whitelist/add", "dest": "/functions/add.js" },
-    { "src": "/api/whitelist/remove", "dest": "/functions/remove.js" }
+    {
+      "src": "/api/whitelist",
+      "dest": "/api/whitelist.json"
+    },
+    {
+      "src": "/api/update",
+      "dest": "/functions/update.js"
+    },
+    {
+      "src": "/api/add",
+      "dest": "/functions/add.js"
+    },
+    {
+      "src": "/api/remove",
+      "dest": "/functions/remove.js"
+    }
   ]
 }
